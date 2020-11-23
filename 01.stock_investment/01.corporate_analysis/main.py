8d6a46dde907f895ebfa9ba81b93cee52381eb7c
predora005 <46834065+predora005@users.noreply.github.com>
2020-11-23 06:59:31 +0000

    [add] 複数銘柄の株価指標を統合し統計量を算出する処理を追加 複数銘柄の株価指標を統合し統計量を算出するための処理として下記を追加。 - カンマや単位などを削除し数値に変換する処理 - 複数銘柄のデータをDataFrameに格納し統計量を算出する処理
     Changes to be committed:
    	modified:   01.stock_investment/01.corporate_analysis/main.py
    	modified:   01.stock_investment/01.corporate_analysis/stinfo/company_info.py

--- 01.stock_investment/01.corporate_analysis/main.py
+++ 01.stock_investment/01.corporate_analysis/main.py
@@ -1,9 +1,8 @@
 # coding: utf-8
 
-import os
-import datetime
-import time
 from stinfo import *
+import pandas as pd
+import re
 
 ##################################################
 # メイン
@@ -15,18 +14,45 @@ if __name__ == '__main__':
         'JR西日本'  : 9022, 
         'JR東海'    : 9021, 
         '東急'      : 9005, 
-        '近鉄GHD'   : 9041,
+        '近鉄'      : 9041,
     }
     
+    # 指定した複数銘柄の基本情報を取得する。
     df = get_basic_infos(codes)
     print(df)
     
-    #for code in codes:
-    #    basic_info = get_basic_info(code)
-    #    print(basic_info)
-    #    
-    #    # 1秒ディレイ
-    #    time.sleep(1)
-        
+    # DataFrameから単位を削る。
+    df2 = trim_unit_from_dataframe(df)
+    print(df2)
+    print(df2.dtypes)
     
+    # 統計量(平均値と標準偏差)を算出する。
+    statistics = pd.DataFrame({'平均値': df2.mean(), '標準偏差': df2.std()})
+    print(statistics)
+    
+    # 各銘柄のデータと統計量を結合する。
+    df3 = df2.append(statistics.T)
+    
+    # 出来高と時価総額の単位を変換する。
+    df3['出来高'] = df3['出来高'] / 1.0e+3
+    df3['時価総額'] = df3['時価総額'] / 1.0e+12
+    df3 = df3.rename(columns={'出来高': '出来高(千株)', '時価総額': '時価総額(兆円)'})
+    
+    # 不要な列を削除する。
+    df3 = df3.drop(columns=['始値', '高値', '安値', '単元株数', '発行済株数', '購入金額'])
+    print(df3)
+    
+    #jre_dict = get_basic_info(9020)
+    #jrw_dict = get_basic_info(9022)
+    #jrc_dict = get_basic_info(9021)
+    #tokyu_dict = get_basic_info(9005)
+    #kintetsu_dict = get_basic_info(9041)
+    #
+    #jre_sr = pd.Series(jre_dict.values(), index=jre_dict.keys(), name='JR東日本')
+    #jrw_sr = pd.Series(jrw_dict.values(), index=jrw_dict.keys(), name='JR西日本')
+    #jrc_sr = pd.Series(jrc_dict.values(), index=jrc_dict.keys(), name='JR東海')
+    #tokyu_sr = pd.Series(tokyu_dict.values(), index=tokyu_dict.keys(), name='東急')
+    #kintetsu_sr = pd.Series(kintetsu_dict.values(), index=kintetsu_dict.keys(), name='近鉄')
+    #
+    #df = pd.DataFrame([jre_sr, jrw_sr, jrc_sr, tokyu_sr, kintetsu_sr])
     
--- 01.stock_investment/01.corporate_analysis/stinfo/company_info.py
+++ 01.stock_investment/01.corporate_analysis/stinfo/company_info.py
@@ -3,7 +3,9 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import numpy as np
 import pandas as pd
+import re
 
 ##############################
 # 指定した複数銘柄の基本情報を取得する
@@ -26,8 +28,7 @@ def get_basic_infos(codes):
         
         # ディクショナリからSeriesを生成
         sr = pd.Series(basic_info.values(), index=basic_info.keys(), name=name)
-        print(sr)
-        
+
         if basic_df is None:
             basic_df = pd.DataFrame([sr])
         else:
@@ -35,7 +36,8 @@ def get_basic_infos(codes):
         
         # 1秒ディレイ
         time.sleep(1)
-        
+    
+    
     return basic_df
 
 ##############################
@@ -83,5 +85,69 @@ def get_basic_info(code):
         
     return basic_info
     
-
+##############################
+# DataFrameから単位を削る。
+##############################
+def trim_unit_from_dataframe(df):
+    """ DataFrameから単位を削る。
     
+    Args:
+        df  (DataFrame) : データフレーム
+
+    Returns:
+        new_df  : 単位削除後のDataFrame
+    """
+    
+    # 単位を削除する関数
+    def trim_unit(x):
+        
+        # 単位=円を削除
+        yen_re = re.search(r"(\d{1,3}(,\d{3})*\.\d+)円", x)
+        if yen_re:
+            value = yen_re.group(1)
+            value = value.replace(',', '')
+            return np.float64(value)
+        
+        # 単位=%を削除
+        per_re = re.search(r"(\d+\.\d+)%", x)
+        if per_re:
+            value = per_re.group(1)
+            return np.float64(value)
+        
+        # 単位=株を削除
+        st_re = re.search(r"(\d{1,3}(,\d{3})*)株", x)
+        if st_re:
+            value = st_re.group(1)
+            value = value.replace(',', '')
+            return np.int64(value)
+        
+        # 単位=倍を削除
+        times_re = re.search(r"(\d+\.\d+)倍", x)
+        if times_re:
+            value = times_re.group(1)
+            return np.float64(value)
+        
+        # 単位=百万円を削除
+        million_yen_re = re.search(r"(\d{1,3}(,\d{3})*)百万円", x)
+        if million_yen_re:
+            value = million_yen_re.group(1)
+            value = value.replace(',', '')
+            value = np.int64(value) * 1000000
+            return value
+        
+        # 単位=千株を削除
+        thousand_st_re = re.search(r"(\d{1,3}(,\d{3})*)千株", x)
+        if thousand_st_re:
+            value = thousand_st_re.group(1)
+            value = value.replace(',', '')
+            value = np.int64(value) * 1000
+            return value
+        
+        return x
+    
+    # 各列に対して、trim_unitを適用する
+    new_df = df.copy()
+    for col in df.columns:
+        new_df[col] = df[col].map(lambda v : trim_unit(v))
+
+    return new_df
