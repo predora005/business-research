98bed95d147d808f8658085443c651525caf83bc
predora005 <46834065+predora005@users.noreply.github.com>
2021-01-26 15:51:40 +0000

    [update] 統計情報取得で取得したデータをJSONからpandasのDataFrameに変換
     Changes to be committed:
    	modified:   02.estat_api/01.how_to_use/main.py

--- 02.estat_api/01.how_to_use/main.py
+++ 02.estat_api/01.how_to_use/main.py
@@ -2,7 +2,7 @@
 
 import sys
 import requests
-#import pandas as pd
+import pandas as pd
 
 ##################################################
 # メイン
@@ -26,14 +26,17 @@ if __name__ == '__main__':
     
     # 統計表情報取得
     json = requests.get(url).json()
+    print('==================================================')
+    print(json)
     
-    # 統計表情報表示
-    datalist = json['GET_STATS_LIST']['DATALIST_INF']
+    # 統計表情報から各表のデータ部取得
+    datalist = json['GET_STATS_LIST']['DATALIST_INF']['LIST_INF']
+    print('==================================================')
     print(datalist)
     
+    # pandasのDataFrameに変換
+    df = pd.DataFrame(datalist)
+    print('==================================================')
+    print(df.head())
     
-    #df = pd.DataFrame(datalist)
-    #print(df.head())
     
-    
-
