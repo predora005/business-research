4f3c6948a7a4b9f169b23326deb482646c849e09
predora005 <46834065+predora005@users.noreply.github.com>
2021-03-31 12:10:59 +0000

    [update] 株価を取得・保存する処理を追加
     Changes to be committed:
    	modified:   01.stock_investment/04.macd/test_macd.py

--- 01.stock_investment/04.macd/test_macd.py
+++ 01.stock_investment/04.macd/test_macd.py
@@ -2,7 +2,9 @@
 
 from pltinit import *
 from stock import *
+import pandas as pd
 import datetime
+import os
 
 ##################################################
 # メイン
@@ -12,10 +14,47 @@ if __name__ == '__main__':
     #  matplotlibの日本語フォント設定処理
     plt_font_init()
     
-    # 指定した銘柄コードの株価を取得する
+    # 銘柄コード
     code = '9020.JP'
-    start_date = datetime.datetime(2021, 3, 29)
-    end_date   = datetime.datetime(2021, 3, 31)
+    
+    # 指定した銘柄コードの株価を読み込む
+    dirpath = os.getcwd()
+    filename = code + '.pickle'
+    filepath = os.path.join(dirpath, filename)
+    exist_file = os.path.isfile(filepath)
+    
+    if exist_file:
+        stored_df = pd.read_pickle(filepath)
+        print('==========')
+        print(stored_df)
+        
+        latest_date = stored_df.index.max()
+        print('==========')
+        print(latest_date)
+        
+        start_date = latest_date + datetime.timedelta(days=1)
+        end_date = start_date + datetime.timedelta(days=1)
+        
+    else:
+        # ファイルが無い場合は、直近5日間の株価を取得する
+        stored_df = None
+        #end_date = datetime.datetime.now()
+        end_date = datetime.datetime(2021, 3, 1)
+        start_date = end_date + datetime.timedelta(days=-1)
+    
+    # 指定した銘柄コードの株価を取得する
     df = get_stock_prices(code, start_date, end_date)
+    print('==========')
     print(df)
     
+    # 既存のデータと新たに取得したデータを結合する
+    if stored_df is None:
+        new_stored_df = df
+    else:
+        new_stored_df = pd.concat([df, stored_df])
+    print('==========')
+    print(new_stored_df)
+        
+        
+    # 株価をファイルに保存する
+    new_stored_df.to_pickle(filepath)
